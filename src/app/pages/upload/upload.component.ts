907e4dfd098e0a7a666c96a7e44b73a7f55b77cd
Youmbi-yvan4 <ngongang.yvan@institutsaintjean.org>
2025-01-16 13:10:06 +0100

    nice

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -16,6 +16,7 @@ export class UploadComponent implements OnInit {
   uploadProgress = 0;
   plagiarismResultsVisible = false;
   similarityResults: any[] = [];
+  inspirationResult: string = '';
   currentTab: string = 'code'; // Default tab
 
   constructor(private http: HttpClient) {}
@@ -75,21 +76,44 @@ export class UploadComponent implements OnInit {
         formData.append('files', file, file.name);
       });
 
-      this.http.post('http://127.0.0.1:5000/upload', formData).subscribe(
-        (response: any) => {
-          this.isUploading = false;
-          this.plagiarismResultsVisible = true;
-          const fileUrls = response.file_urls; // Get the file URLs from the response
-          this.checkSimilarities(fileUrls); // Pass the URLs to the similarity check method
-        },
-        (error) => {
-          console.error('Upload error:', error);
-          this.isUploading = false;
-        }
-      );
+      if (this.uploadedFiles.length === 2) {
+        this.uploadFilesForInspiration(formData);
+      } else {
+        this.uploadFilesForSimilarity(formData);
+      }
     } else {
       alert('No files selected for upload');
     }
+  }
+
+  uploadFilesForSimilarity(formData: FormData) {
+    this.http.post('http://127.0.0.1:5000/upload', formData).subscribe(
+      (response: any) => {
+        this.isUploading = false;
+        this.plagiarismResultsVisible = true;
+        const fileUrls = response.file_urls; // Get the file URLs from the response
+        this.checkSimilarities(fileUrls); // Pass the URLs to the similarity check method
+      },
+      (error) => {
+        console.error('Upload error:', error);
+        this.isUploading = false;
+      }
+    );
+  }
+
+  uploadFilesForInspiration(formData: FormData) {
+    this.http.post('http://127.0.0.1:5000/upload', formData).subscribe(
+      (response: any) => {
+        this.isUploading = false;
+        this.plagiarismResultsVisible = true;
+        const fileUrls = response.file_urls; // Get the file URLs from the response
+        this.checkInspiration(fileUrls); // Pass the URLs to the inspiration check method
+      },
+      (error) => {
+        console.error('Upload error:', error);
+        this.isUploading = false;
+      }
+    );
   }
 
   checkSimilarities(fileUrls: string[]) {
@@ -103,9 +127,27 @@ export class UploadComponent implements OnInit {
     );
   }
 
+  checkInspiration(fileUrls: string[]) {
+    this.http.post('http://127.0.0.1:5000/inspiration', { file_paths: fileUrls }).subscribe(
+      (response: any) => {
+        this.displayInspirationResult(response);
+      },
+      (error) => {
+        console.error('Inspiration check error:', error);
+      }
+    );
+  }
+
   displayPlagiarismResults(response: any) {
     console.log('Plagiarism results:', response);
     this.similarityResults = response;
+  }
+
+  displayInspirationResult(response: any) {
+    console.log('Inspiration result:', response);
+    const file1 = this.uploadedFiles[0].name;
+    const file2 = this.uploadedFiles[1].name;
+    this.inspirationResult = `${file1} is inspired ${response.inspiration_percentage.toFixed(2)}% by ${file2}`;
   }
 
   switchTab(tab: string): void {
